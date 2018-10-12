0ff6491f710fdad6982108094e7331123a33c4fa
石磊 <syl@shileideMacBook-Pro.local>
2018-10-12 09:40:13 +0800

    更新数据库，添加字段

--- library/src/main/java/com/hm/iou/database/table/IouData.java
+++ library/src/main/java/com/hm/iou/database/table/IouData.java
@@ -86,6 +86,10 @@ public class IouData extends SugarRecord {
     private String othersMobile;
 
 
+    private int fieldFour;      //预留字段4 平台借条为安全指数，为0的时候表示安全指数未知
+    private String fieldOne;    //预留字段1 平台借条为agencyId 机构id AgencyType为1,2时关联至T_IOUAgencyPlatform 当AgencyType为3时,其为相应的枚举 ,
+    private int fieldThree;     //预留字段3 平台借条为AgencyType 1-p2p 2-信用卡 3-房贷/抵押贷/信用贷 4-自定义 ,
+
     public String getIouId() {
         return iouId;
     }
@@ -415,6 +419,30 @@ public class IouData extends SugarRecord {
 
     public void setOthersMobile(String othersMobile) {
         this.othersMobile = othersMobile;
+    }
+
+    public int getFieldFour() {
+        return fieldFour;
+    }
+
+    public void setFieldFour(int fieldFour) {
+        this.fieldFour = fieldFour;
+    }
+
+    public String getFieldOne() {
+        return fieldOne;
+    }
+
+    public void setFieldOne(String fieldOne) {
+        this.fieldOne = fieldOne;
+    }
+
+    public int getFieldThree() {
+        return fieldThree;
+    }
+
+    public void setFieldThree(int fieldThree) {
+        this.fieldThree = fieldThree;
     }
 
     /**
