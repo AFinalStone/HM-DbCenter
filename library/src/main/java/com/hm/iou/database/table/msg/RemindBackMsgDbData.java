af6f998ba84bd246901eb75d2848055b54928c31
石磊 <syl@shileideMacBook-Pro.local>
2019-04-24 14:04:31 +0800

    修改提醒归还数据库对象

--- library/src/main/java/com/hm/iou/database/table/msg/RemindBackMsgDbData.java
+++ library/src/main/java/com/hm/iou/database/table/msg/RemindBackMsgDbData.java
@@ -3,32 +3,28 @@ package com.hm.iou.database.table.msg;
 import com.orm.SugarRecord;
 
 public class RemindBackMsgDbData extends SugarRecord {
-
     /**
-     * content : string
-     * createTime : 2019-04-15T12:17:23.987Z
+     * createTime : 2019-04-24T01:50:35.115Z
      * iouKind : 0
      * jumpUrl : string
-     * repayAmount : 0
-     * repayDateTime : 2019-04-15T12:17:23.988Z
+     * repayDateTime : 2019-04-24T01:50:35.115Z
+     * repayThing : string
+     * thingsType : 0
      * title : string
      */
 
-    private String content;
+    /**
+     * repayAmount : 0
+     * title : string
+     */
+
     private String createTime;
-    private int iouKind;
+    private int iouKind;//借条类型: 1:资金借条, 3:娱乐借条, 4:娱乐借条插画,5:平台借条,7:纸质借条,8:纸质收条,9:电子收条,11:资金借条V2 ,
     private String jumpUrl;
-    private int repayAmount;
     private String repayDateTime;
+    private String repayThing;
+    private int thingsType;// 物品类别:0=物品，1=资金 ,
     private String title;
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
     public String getCreateTime() {
         return createTime;
@@ -54,20 +50,28 @@ public class RemindBackMsgDbData extends SugarRecord {
         this.jumpUrl = jumpUrl;
     }
 
-    public int getRepayAmount() {
-        return repayAmount;
-    }
-
-    public void setRepayAmount(int repayAmount) {
-        this.repayAmount = repayAmount;
-    }
-
     public String getRepayDateTime() {
         return repayDateTime;
     }
 
     public void setRepayDateTime(String repayDateTime) {
         this.repayDateTime = repayDateTime;
+    }
+
+    public String getRepayThing() {
+        return repayThing;
+    }
+
+    public void setRepayThing(String repayThing) {
+        this.repayThing = repayThing;
+    }
+
+    public int getThingsType() {
+        return thingsType;
+    }
+
+    public void setThingsType(int thingsType) {
+        this.thingsType = thingsType;
     }
 
     public String getTitle() {
@@ -77,4 +81,6 @@ public class RemindBackMsgDbData extends SugarRecord {
     public void setTitle(String title) {
         this.title = title;
     }
+
+
 }
